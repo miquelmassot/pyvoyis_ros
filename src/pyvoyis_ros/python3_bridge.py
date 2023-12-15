244b2b2d9ceb25f804c064c4ba3014932a90bfc5
Miquel Massot <miquel.massot@gmail.con>
2023-12-15 14:48:21 +0000

    Implement safety thread to check cameras are acquiring

--- src/pyvoyis_ros/python3_bridge.py
+++ src/pyvoyis_ros/python3_bridge.py
@@ -21,9 +21,14 @@ class PyVoyisXMLRPCServer:
 
         self.api = None
         self.asked_to_run = False
+        self.asked_to_acquire = False
+        self.time_since_request_s = 0
+        self.timeout_s = 60
 
         self.run_thread = threading.Thread(target=self.api_thread)
         self.run_thread.start()
+        self.safety_thread = threading.Thread(target=self.safety_thread)
+        self.safety_thread.start()
 
         self.server.register_function(self.run)
         self.server.register_function(self.kill)
@@ -40,8 +45,33 @@ class PyVoyisXMLRPCServer:
         self.api = VoyisAPI(self.config)
         self.api.run()
 
+    def safety_thread(self):
+        try:
+            while True:
+                if not self.asked_to_acquire:
+                    continue
+
+                self.time_since_request_s += 1
+
+                if self.time_since_request_s < self.timeout_s:
+                    continue
+
+                if self.api.state.is_acquiring:
+                    continue
+
+                print("Safety timeout reached, restarting Voyis API")
+                self.api.shutdown()
+                self.api = VoyisAPI(self.config)
+                self.api.run()
+                print("Restarted Voyis API, requesting acquisition...")
+                self.api.request_acquisition()
+                self.time_since_request_s = 0
+                time.sleep(1)
+        except KeyboardInterrupt:
+            pass
+
     def run(self, configuration_file):
-        print("RUN called")
+        # Called by pyvoyis_ros_node.py at initialization
         self.config = Configuration(configuration_file)
         self.server.register_function(self.start_acquisition)
         self.server.register_function(self.stop_acquisition)
@@ -49,16 +79,22 @@ class PyVoyisXMLRPCServer:
         return True
 
     def kill(self):
+        # Called by pyvoyis_ros_node.py at ROS shutdown
         if self.api:
             self.api.shutdown()
 
     def start_acquisition(self):
+        # Called by pyvoyis_ros_node.py when start_acquisition service is called
         print("start_acquisition called")
+        self.time_since_request_s = 0
+        self.asked_to_acquire = True
         self.api.request_acquisition()
         return True
 
     def stop_acquisition(self):
+        # Called by pyvoyis_ros_node.py when stop_acquisition service is called
         print("stop_acquisition called")
+        self.asked_to_acquire = False
         self.api.request_stop()
         return True
 
--- src/pyvoyis_ros/pyvoyis_ros_node.py
+++ src/pyvoyis_ros/pyvoyis_ros_node.py
@@ -16,20 +16,20 @@ class VoyisROS:
         # Get parameters
         configuration_file = rospy.get_param(
             "~configuration_file")
-        
+
         time.sleep(5)
         print("Trying to connect to http://localhost:8000")
 
-        self.xmlrpc_server = xmlrpclib.ServerProxy('http://localhost:8000')    
+        self.xmlrpc_server = xmlrpclib.ServerProxy('http://localhost:8000')
         print("Trying to call RUN...")
         while True:
-            try:    
+            try:
                 self.xmlrpc_server.run(configuration_file)
                 time.sleep(1)
             except socket.error, exc:
-                print "Caught exception socket.error : %s" % exc    
+                print "Caught exception socket.error : %s" % exc
             break
-        
+
 
         # Offer start_acquisition service
         rospy.Service("~start_acquisition", Empty, self.start_acquisition_srv_cb)
